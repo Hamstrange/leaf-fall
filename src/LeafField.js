60f0c2bd0e9268523a7577662d05e96cd1915379
Hamstrange <minecrafterpavel@gmail.com>
2026-04-08 17:44:50 +0700

    refactor: extract GPGPU logic into separate GPGPUSimulation class

--- src/LeafField.js
+++ src/LeafField.js
@@ -1,15 +1,12 @@
 // src/LeafField.js
 import * as THREE from 'three';
-import { GPUComputationRenderer } from 'three/addons/misc/GPUComputationRenderer.js';
 import { createLeafGeometry, createLeafMaterial } from './newLeafModel.js';
-import { velocityShader, angVelShader, positionShader, rotationShader, hingeShader } from './shaders/leafShaders.js';
-import { initLeafTextures } from './utils/initLeafTextures.js';
-import { initLeafUniforms } from './utils/initLeafUniforms.js';
-import { setGPGPUDependencies } from './utils/setGPGPUDependencies.js';
 import { defaultPhysicsParams } from './config/physicsParams.js';
 import { createLeafInstances } from './utils/createLeafInstances.js';
+import { initLeafUniforms } from './utils/initLeafUniforms.js';
 import { updateUniforms } from './utils/updateUniforms.js';
 import { addGUIMethods } from './mixins/leafFieldGUIMethods.js';
+import { GPGPUSimulation } from './GPGPUSimulation.js';
 
 export class LeafField {
     constructor(gridSize, scene, renderer) {
@@ -24,43 +21,16 @@ export class LeafField {
 
         this.params = { ...defaultPhysicsParams };
 
-        this.gpuCompute = new GPUComputationRenderer(this.gridSize, this.gridSize, this.renderer);
+        this.simulation = new GPGPUSimulation(gridSize, renderer, this.params);
 
-        this.posTexture = this.gpuCompute.createTexture();
-        this.velTexture = this.gpuCompute.createTexture();
-        this.rotTexture = this.gpuCompute.createTexture();
-        this.angVelTexture = this.gpuCompute.createTexture();
-        this.hingeTexture = this.gpuCompute.createTexture();
-        this.paramsTexture = this.gpuCompute.createTexture();
+        // Сохраняем ссылки на GPGPU-переменные для GUI и uniform'ов
+        this.velocityVar = this.simulation.getVelocityVar();
+        this.angVelVar = this.simulation.getAngVelVar();
+        this.positionVar = this.simulation.getPositionVar();
+        this.rotationVar = this.simulation.getRotationVar();
+        this.hingeVar = this.simulation.getHingeVar();
 
-        initLeafTextures({
-            numLeaves: this.numLeaves,
-            params: this.params,
-            posTexture: this.posTexture,
-            velTexture: this.velTexture,
-            rotTexture: this.rotTexture,
-            angVelTexture: this.angVelTexture,
-            hingeTexture: this.hingeTexture,
-            paramsTexture: this.paramsTexture
-        });
-
-        this.velocityVar = this.gpuCompute.addVariable('textureVelocity', velocityShader, this.velTexture);
-        this.angVelVar = this.gpuCompute.addVariable('textureAngularVelocity', angVelShader, this.angVelTexture);
-        this.positionVar = this.gpuCompute.addVariable('texturePosition', positionShader, this.posTexture);
-        this.rotationVar = this.gpuCompute.addVariable('textureRotation', rotationShader, this.rotTexture);
-        this.hingeVar = this.gpuCompute.addVariable('textureHinge', hingeShader, this.hingeTexture);
-
-        setGPGPUDependencies(this.gpuCompute, {
-            velocityVar: this.velocityVar,
-            angVelVar: this.angVelVar,
-            positionVar: this.positionVar,
-            rotationVar: this.rotationVar,
-            hingeVar: this.hingeVar
-        });
-
-        const error = this.gpuCompute.init();
-        if (error) console.error('GPGPU init error:', error);
-
+        // Инициализация uniform'ов GPGPU переменных
         initLeafUniforms({
             velocityVar: this.velocityVar,
             angVelVar: this.angVelVar,
@@ -69,10 +39,11 @@ export class LeafField {
             hingeVar: this.hingeVar
         }, this.params);
 
-        this.material.uniforms.texturePosition.value = this.posTexture;
-        this.material.uniforms.textureRotation.value = this.rotTexture;
-        this.material.uniforms.textureHinge.value = this.hingeTexture;
-        this.material.uniforms.textureParams.value = this.paramsTexture;
+        // Передаём начальные текстуры в материал
+        this.material.uniforms.texturePosition.value = this.simulation.posTexture;
+        this.material.uniforms.textureRotation.value = this.simulation.rotTexture;
+        this.material.uniforms.textureHinge.value = this.simulation.hingeTexture;
+        this.material.uniforms.textureParams.value = this.simulation.paramsTexture;
     }
 
     update(delta, time) {
@@ -84,12 +55,14 @@ export class LeafField {
             hingeVar: this.hingeVar
         }, delta, time);
 
-        this.gpuCompute.compute();
+        // Запускаем GPGPU вычисления
+        this.simulation.compute();
 
-        this.material.uniforms.texturePosition.value = this.gpuCompute.getCurrentRenderTarget(this.positionVar).texture;
-        this.material.uniforms.textureRotation.value = this.gpuCompute.getCurrentRenderTarget(this.rotationVar).texture;
-        this.material.uniforms.textureHinge.value = this.gpuCompute.getCurrentRenderTarget(this.hingeVar).texture;
-        this.material.uniforms.textureParams.value = this.paramsTexture;
+        // Получаем обновлённые текстуры
+        const textures = this.simulation.getCurrentTextures();
+        this.material.uniforms.texturePosition.value = textures.position;
+        this.material.uniforms.textureRotation.value = textures.rotation;
+        this.material.uniforms.textureHinge.value = textures.hinge;
     }
 }
 
